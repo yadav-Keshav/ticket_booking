fefbb03cc4f5ef78a330f7d7c22178c18619b935
yadav-Keshav <keshav.nita2019@gmail.com>
2022-08-05 00:33:41 +0530

    error resolve

---
+++ Backend/index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+dotenv.config();
+
+const MONGO=process.env.MONGO;
+const connect = async () => {
+    mongoose.connect(MONGO)
+        .then((db) => { console.log(`sucefully Connected to : ${db.connection.host}`) })
+        .catch((err) => { console.log(err.message) })
+};
+
+
+
+//middlewares
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong!";
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack,
+    });
+});
+const PORT = process.env.PORT || 8800;
+app.listen(PORT, () => {
+    connect();
+    console.log(`Server is Listening on Port : ${PORT}`);
+})
---
+++ Frontend/client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import "./navbar.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+const Navbar = () => {
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">Booking.com</span>
+        </Link>
+        {user ? user.username : (
+          <div className="navItems">
+            <button onClick={() => navigate("/register")} className="navButton">Register</button>
+            <button onClick={() => navigate("/login")} className="navButton">Login</button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
