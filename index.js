dd4b71db2218d76d777c52ff062c3b965d0b7c10
yadav-Keshav <keshav.nita2019@gmail.com>
2022-08-05 23:52:49 +0530

    Procfile added

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const connect = async () => {
 app.use(cors())
 app.use(cookieParser())
 app.use(express.json());
-app.length("/",(req,res)=>{
+app.get("/",(req,res)=>{
     return res.status(200).json({message:"Sever is working"})
 })
 app.use("/api/auth", authRoute);
